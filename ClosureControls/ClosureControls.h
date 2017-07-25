557ff590af7181ccd6a2bb92a94351eb127e1317
Marco Conti <marcoconti83@gmail.com>
2017-07-25 18:23:01 +0200

    Initial commit

---
+++ ClosureControls/ClosureControls.h
@@ -0,0 +1,19 @@
+//
+//  ClosureControls.h
+//  ClosureControls
+//
+//  Created by Marco Conti on 25.07.17.
+//  Copyright © 2017 com.marco83. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ClosureControls.
+FOUNDATION_EXPORT double ClosureControlsVersionNumber;
+
+//! Project version string for ClosureControls.
+FOUNDATION_EXPORT const unsigned char ClosureControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClosureControls/PublicHeader.h>
+
+
